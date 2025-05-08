2f1938da4d855308486e582abeeab050f3334fa3
Anubhav Singh <165538800+anuvsingh@users.noreply.github.com>
2025-05-08 10:34:06 +0530

    Update IndexOfMethod.js

--- 03_JavaScript/JS by Shraddha Ma'am/06_String Methods/IndexOfMethod.js
+++ 03_JavaScript/JS by Shraddha Ma'am/06_String Methods/IndexOfMethod.js
@@ -3,4 +3,4 @@ let str = "ILoveJava"
 console.log(str.indexOf("Love"))    //1 means found
 console.log(str.indexOf("C"))    //-1 means not found
 console.log(str.indexOf("o"))    //2 (only if 1 index)
-console.log(str.indexOf(""))    //0 Why?
+console.log(str.indexOf(""))    //0 Why? - In JavaScript, when you use .indexOf("") on a string, it searches for an empty string. And here's the key rule: An empty string "" is considered to be present at every index in a string, including the start. But when indexOf("") is called, it returns the first valid index where the empty string can be found — and that is always 0.
